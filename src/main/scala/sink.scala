b799fce693a5c0abe6e75301986bc9c614932e27
Kyle Ong <kho226@nyu.edu>
2019-02-02 19:09:13 -0500

    officially parsing data from s3 ~ reading from persons-avro

--- src/main/scala/sink.scala
+++ src/main/scala/sink.scala
@@ -2,12 +2,14 @@ package proj
 
 import java.time.{LocalDate, Period}
 
-import proj.Constants
+import proj.Constants.reddit_schema
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.{DataTypes, StructType}
+
+import org.apache.spark.rdd.RDD
 
 //elastic search imports
 import com.typesafe.config.ConfigFactory
@@ -15,8 +17,6 @@ import org.elasticsearch.hadoop.cfg.ConfigurationOptions
 
 //redis connectors
 import com.redislabs.provider.redis._
-
-
 
 object StreamsProcessor {
   def main(args: Array[String]): Unit = {
@@ -40,14 +40,8 @@ class StreamsProcessor(brokers: String) {
   private val outputMode = config.getString("spark.elasticsearch.output.mode")
   private val destination = config.getString("spark.elasticsearch.data.source")
   private val checkpointLocation = config.getString("spark.elasticsearch.checkpoint.location")
-  private val index = config.getString("spark.elasticsearch.index")
   private val docType = config.getString("spark.elasticsearch.doc.type")
-  private val indexAndDocType = s"$index/$docType"
-
-  private val redisConfig = new RedisConfig(new RedisEndpoint(config.getString("spark.redis.redisServerDnsAddress"),
-                                                              6379,
-                                                              config.getString("spark.redis.redisPassword")))
-
+  //private val indexAndDocType = s"$index/$docType"
 
   def process(): Unit = {
 
@@ -60,29 +54,40 @@ class StreamsProcessor(brokers: String) {
       .master(master)
       .getOrCreate()
 
+    import spark.implicits._
 
-   val df = spark.readStream
-        .format("kafka")
-        .option("kafka.bootstrap.servers", brokers)
-        .option("subscribe", "persons-avro")
-        .load()
+    val stream_df = spark.readStream
+                          .format("kafka")
+                          .option("kafka.bootstrap.servers", brokers)
+                          .option("subscribe", "persons-avro")
+                          .load()
 
 
-    val query = df.writeStream
-          .outputMode("append")
-          .format("console")
-          .start()
+    //val query_1= stream_df.writeStream
+    //                     .outputMode("append")
+    //                     .format("console")
+    //                     .start()
 
-    val es_df = df.selectExpr("CAST(VALUE as STRING)")
+    val df = stream_df.selectExpr("CAST(value as STRING)")
 
-    es_df.writeStream
-         .outputMode(outputMode)
-         .format(destination)
-         .option("checkpointLocation", checkpointLocation)
-         .start(indexAndDocType)
-         .awaitTermination()
 
-  }
+    val reddit_df = df.select(from_json('value, reddit_schema ) as 'reddit_comment)
 
-}
+    val query_2 = reddit_df.writeStream
+                           .outputMode("append")
+                           .format("console")
+                           .start()
 
+
+    //reddit_df.writeStream
+    //       .outputMode(outputMode)
+    //       .format(destination)
+    //       .option("checkpointLocation", checkpointLocation)
+    //       .start("reddit-comments/personal")
+    //       .awaitTermination()
+
+
+    reddit_df.printSchema()
+
+    //query_1.awaitTermination()
+    query_2.awaitTermination()
