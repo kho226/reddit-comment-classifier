5f4b86d91234cefd1d510d272f8cf334c4840797
Kyle Ong <kho226@nyu.edu>
2019-02-08 14:32:04 -0500

    implemented templates

--- src/frontend/app/main/routes.py
+++ src/frontend/app/main/routes.py
@@ -1,6 +1,8 @@
 from app.main import bp
+from flask import render_template
 
 @bp.route('/')
 @bp.route('/index')
 def index():
-    return "Hello, World!"
+    user = {"username": "Kyle"}
+    return render_template("index.html",  title="Reddit Trends", user=user)
