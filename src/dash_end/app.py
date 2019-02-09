cfbc6c4305d61a549e999732df58392dfa71fed8
Kyle Ong <kho226@nyu.edu>
2019-02-08 19:02:45 -0500

    bubble chart we lit

--- src/dash_end/app.py
+++ src/dash_end/app.py
@@ -1,20 +1,14 @@
-import datetime
-
+# -*- coding: utf-8 -*-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly
+import plotly as py
 import plotly.graph_objs as go
-from dash.dependencies import Input, Output
-    
-# pip install pyorbital
-from pyorbital.orbital import Orbital
+
 
 import constants
 import redis
 from elasticsearch import Elasticsearch
-
-satellite = Orbital('TERRA')
 
 es = Elasticsearch(constants.elastic["url"])
 redis = redis.StrictRedis(
@@ -25,81 +19,38 @@ redis = redis.StrictRedis(
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.layout = html.Div(
-    html.Div([
-        html.H4('TERRA Satellite Live Feed'),
-        html.Div(id='live-update-text'),
-        dcc.Graph(id='live-update-graph'),
-        dcc.Interval(
-            id='interval-component',
-            interval=1*1000, # in milliseconds
-            n_intervals=0
-        )
-    ])
+size = [20, 40, 60, 80, 100, 80, 60, 40, 20, 40]
+trace0 = go.Scatter(
+    x=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    y=[11, 12, 10, 11, 12, 11, 12, 13, 12, 11],
+    mode='markers',
+    marker=dict(
+        size=size,
+        sizemode='area',
+        sizeref=2.*max(size)/(40.**2),
+        sizemin=4
+    )
 )
 
-
-@app.callback(Output('live-update-text', 'children'),
-              [Input('interval-component', 'n_intervals')])
-def update_metrics(n):
-    lon, lat, alt = satellite.get_lonlatalt(datetime.datetime.now())
-    style = {'padding': '5px', 'fontSize': '16px'}
-    return [
-        html.Span('Longitude: {0:.2f}'.format(lon), style=style),
-        html.Span('Latitude: {0:.2f}'.format(lat), style=style),
-        html.Span('Altitude: {0:0.2f}'.format(alt), style=style)
-    ]
+data = [trace0]
 
 
-# Multiple components can update everytime interval gets fired.
-@app.callback(Output('live-update-graph', 'figure'),
-              [Input('interval-component', 'n_intervals')])
-def update_graph_live(n):
-    satellite = Orbital('TERRA')
-    data = {
-        'time': [],
-        'Latitude': [],
-        'Longitude': [],
-        'Altitude': []
-    }
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-    # Collect some data
-    for i in range(180):
-        time = datetime.datetime.now() - datetime.timedelta(seconds=i*20)
-        lon, lat, alt = satellite.get_lonlatalt(
-            time
+app.layout = html.Div(children=[
+    html.H1(children='Trenddit'),
+
+    html.Div(children='''
+        Realtime analytics of trends in reddit topics
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=go.Figure(
+            data
         )
-        data['Longitude'].append(lon)
-        data['Latitude'].append(lat)
-        data['Altitude'].append(alt)
-        data['time'].append(time)
-
-    # Create the graph with subplots
-    fig = plotly.tools.make_subplots(rows=2, cols=1, vertical_spacing=0.2)
-    fig['layout']['margin'] = {
-        'l': 30, 'r': 10, 'b': 30, 't': 10
-    }
-    fig['layout']['legend'] = {'x': 0, 'y': 1, 'xanchor': 'left'}
-
-    fig.append_trace({
-        'x': data['time'],
-        'y': data['Altitude'],
-        'name': 'Altitude',
-        'mode': 'lines+markers',
-        'type': 'scatter'
-    }, 1, 1)
-    fig.append_trace({
-        'x': data['Longitude'],
-        'y': data['Latitude'],
-        'text': data['time'],
-        'name': 'Longitude vs Latitude',
-        'mode': 'lines+markers',
-        'type': 'scatter'
-    }, 2, 1)
-
-    return fig
-
+    )
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
